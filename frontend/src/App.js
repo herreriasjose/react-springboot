fbfe3d4ca08adb34c1c058ad5400489af3fe88e9
herreriasjose <herreriasjose@gmail.com>
2018-11-22 18:26:27 +0100

    Add tools to create production build.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {};
+
+  componentDidMount() {
+    setInterval(this.hello, 1000);
+}
+
+
+ hello = () => {
+  fetch('/api/time')
+  .then( res => res.text())
+  .then(res => { this.setState( {message: res})
+  });
+};
+    
+ 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <h1 className="App-title">{this.state.message}</h1>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
         </header>
       </div>
     );
